cd5398d7559a99fa313a498908551adbd870ddce
Excelyynx <excelyynxl@gmail.com>
2026-04-10 20:22:27 +0800

    feat(component): pass userInfo from backend to component display

--- frontend/src/components/Cards/ProfileInfo.jsx
+++ frontend/src/components/Cards/ProfileInfo.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import { getInitials } from '../../utils/helper'
 
-const ProfileInfo = ({ onLogout }) => {
+const ProfileInfo = ({ userInfo, onLogout }) => {
+
   return (
     <div className='flex items-center gap-3'>
         <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-550 font-medium bg-slate-100'>
-            {getInitials("John William")}
+            {getInitials(userInfo?.fullName)}
         </div>
 
-        <div>
-            <p className='text-sm font-medium'>
-                John William 
-            </p>
-
-            <button 
-                className='text-sm text-slate-700 underline'
-                onClick={onLogout}
-            >
-                Logout 
-            </button>
-        </div>
+        {userInfo && (
+            <div>
+                <p className='text-sm font-medium'>
+                    {userInfo?.fullName} 
+                </p>
+                <button 
+                    className='text-sm text-slate-700 underline'
+                    onClick={onLogout}
+                >
+                    Logout 
+                </button>
+            </div>
+        )}
     </div>
   )
 }
