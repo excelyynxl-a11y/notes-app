5097306826a27d3920696a78af0167c682591bfc
Excelyynx <excelyynxl@gmail.com>
2026-04-12 11:38:09 +0800

    fix(typo): add key to span element in TagInput

--- frontend/src/components/Input/TagInput.jsx
+++ frontend/src/components/Input/TagInput.jsx
@@ -34,7 +34,7 @@ const TagInput = ({
                 {tags?.length > 0 && (
                     <div className='flex items-center gap-2 flex-wrap mt-2'>
                         {tags.map((tag, index) => (
-                            <span className='flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded'>
+                            <span key={index} className='flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded'>
                                 # {tag}
                                 <button onClick={() => {handleRemoveTag(tag)}}>
                                     <X />
